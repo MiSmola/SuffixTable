97bcb37efb7f6453e010b9a54cebea79517f95f7
djnick <56382718+djnick@users.noreply.github.com>
2020-10-26 20:04:06 +0100

    MiSmola #1 Create File operator classes template.

---
+++ FileExecutor.cpp
@@ -0,0 +1,18 @@
+//
+// Created by djnic on 26.10.2020.
+//
+
+#include "FileExecutor.h"
+
+void FileExecutor::createResultFile() {
+
+}
+
+std::string FileExecutor::loadTextFile() {
+    return std::string();
+}
+
+std::string *FileExecutor::loadPatternFile() {
+    return nullptr;
+}
+
